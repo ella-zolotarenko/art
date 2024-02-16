f8c96bc91f15987b80649aac145f32c3e9993739
olegvolkolupov <olegvolkolupov@gmail.com>
2024-02-16 13:06:39 +0100

    new foto add

--- src/service/paintings.js
+++ src/service/paintings.js
@@ -7,6 +7,33 @@ import img06 from "../img/paintings/06.jpg";
 import img07 from "../img/paintings/07.jpg";
 import img08 from "../img/paintings/08.jpg";
 import img09 from "../img/paintings/09.jpg";
+import img10 from "../img/paintings/10.jpg";
+import img11 from "../img/paintings/11.jpg";
+import img12 from "../img/paintings/12.jpg";
+import img13 from "../img/paintings/13.jpg";
+import img14 from "../img/paintings/14.jpg";
+import img15 from "../img/paintings/15.jpg";
+import img16 from "../img/paintings/16.jpg";
+import img17 from "../img/paintings/17.jpg";
+import img18 from "../img/paintings/18.jpg";
+import img19 from "../img/paintings/19.jpg";
+import img20 from "../img/paintings/20.jpg";
+import img21 from "../img/paintings/21.jpg";
+import img22 from "../img/paintings/22.jpg";
+import img23 from "../img/paintings/23.jpg";
+import img24 from "../img/paintings/24.jpg";
+import img25 from "../img/paintings/25.jpg";
+import img26 from "../img/paintings/26.jpg";
+import img27 from "../img/paintings/27.jpg";
+import img28 from "../img/paintings/28.jpg";
+import img29 from "../img/paintings/29.jpg";
+import img30 from "../img/paintings/30.jpg";
+import img31 from "../img/paintings/31.jpg";
+import img32 from "../img/paintings/32.jpg";
+import img33 from "../img/paintings/33.jpg";
+import img34 from "../img/paintings/34.jpg";
+import img35 from "../img/paintings/35.jpg";
+import img36 from "../img/paintings/36.jpg";
 
 const paintings = [
   {
@@ -62,6 +89,168 @@ const paintings = [
     src: img09,
     title: "petrykivka9",
     alt: "petrykivka9",
+  },
+  {
+    id: 10,
+    src: img10,
+    title: "petrykivka10",
+    alt: "petrykivka10",
+  },
+  {
+    id: 11,
+    src: img11,
+    title: "petrykivka11",
+    alt: "petrykivka11",
+  },
+  {
+    id: 12,
+    src: img12,
+    title: "petrykivka12",
+    alt: "petrykivka12",
+  },
+  {
+    id: 13,
+    src: img13,
+    title: "petrykivka13",
+    alt: "petrykivka13",
+  },
+  {
+    id: 14,
+    src: img14,
+    title: "petrykivka14",
+    alt: "petrykivka14",
+  },
+  {
+    id: 15,
+    src: img15,
+    title: "petrykivka15",
+    alt: "petrykivka15",
+  },
+  {
+    id: 16,
+    src: img16,
+    title: "petrykivka16",
+    alt: "petrykivka16",
+  },
+  {
+    id: 17,
+    src: img17,
+    title: "petrykivka17",
+    alt: "petrykivka17",
+  },
+  {
+    id: 18,
+    src: img18,
+    title: "petrykivka18",
+    alt: "petrykivka18",
+  },
+  {
+    id: 19,
+    src: img19,
+    title: "petrykivka19",
+    alt: "petrykivka19",
+  },
+  {
+    id: 20,
+    src: img20,
+    title: "petrykivka20",
+    alt: "petrykivka20",
+  },
+  {
+    id: 21,
+    src: img21,
+    title: "petrykivka21",
+    alt: "petrykivka21",
+  },
+  {
+    id: 22,
+    src: img22,
+    title: "petrykivka22",
+    alt: "petrykivka22",
+  },
+  {
+    id: 23,
+    src: img23,
+    title: "petrykivka23",
+    alt: "petrykivka23",
+  },
+  {
+    id: 24,
+    src: img24,
+    title: "petrykivka24",
+    alt: "petrykivka24",
+  },
+  {
+    id: 25,
+    src: img25,
+    title: "petrykivka25",
+    alt: "petrykivka25",
+  },
+  {
+    id: 26,
+    src: img26,
+    title: "petrykivka26",
+    alt: "petrykivka26",
+  },
+  {
+    id: 27,
+    src: img27,
+    title: "petrykivka27",
+    alt: "petrykivka27",
+  },
+  {
+    id: 28,
+    src: img28,
+    title: "petrykivka28",
+    alt: "petrykivka28",
+  },
+  {
+    id: 29,
+    src: img29,
+    title: "petrykivka29",
+    alt: "petrykivka29",
+  },
+  {
+    id: 30,
+    src: img30,
+    title: "petrykivka30",
+    alt: "petrykivka30",
+  },
+  {
+    id: 31,
+    src: img31,
+    title: "petrykivka31",
+    alt: "petrykivka31",
+  },
+  {
+    id: 32,
+    src: img32,
+    title: "petrykivka32",
+    alt: "petrykivka32",
+  },
+  {
+    id: 33,
+    src: img33,
+    title: "petrykivka33",
+    alt: "petrykivka33",
+  },
+  {
+    id: 34,
+    src: img34,
+    title: "petrykivka34",
+    alt: "petrykivka34",
+  },
+  {
+    id: 35,
+    src: img35,
+    title: "petrykivka35",
+    alt: "petrykivka35",
+  },
+  {
+    id: 36,
+    src: img36,
+    title: "petrykivka36",
+    alt: "petrykivka36",
   },
 ];
 
